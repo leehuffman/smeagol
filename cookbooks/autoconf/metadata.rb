43e7ef5230efdb6e7c77f38dad89f3651351e96c
Lee Huffman <huffman@github.com>
2012-03-20 14:36:21 -0700

    Fix up autoconf metadata

--- cookbooks/autoconf/metadata.rb
+++ cookbooks/autoconf/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "atmos.org"
+maintainer_email "atmos@atmos.org"
+license          "MIT"
 description      "Installs/Configures autoconf"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
